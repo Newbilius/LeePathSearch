020f816065c4e39209fc478d8626fd5d1a759a2d
newbilius <newbilius@gmail.com>
2018-11-25 14:25:41 +0500

    Рабочий алгоритм

---
+++ DebugHelpers.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace LeePathSearchAlgorithm
+{
+    public static class DebugHelpers
+    {
+        public static void PrintToConsole2DArray(int[,] array)
+        {
+            var width = array.GetLength(0);
+            var height = array.GetLength(1);
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < height; y++)
+                    Console.Write($"{array[x, y],3}\t");
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintToConsole2DCoordinates(int[,] array)
+        {
+            var width = array.GetLength(0);
+            var height = array.GetLength(1);
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < height; y++)
+                    Console.Write($"{x}x{y}\t");
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintPath(int[,] array,
+        PathSearchLee.Point[] path,
+        PathSearchLee.Point start,
+        PathSearchLee.Point end)
+        {
+            var width = array.GetLength(0);
+            var height = array.GetLength(1);
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < height; y++)
+                {
+                    if (x == start.X && y == start.Y)
+                    {
+                        Console.Write($"STR\t");
+                        continue;
+                    }
+                    if (x == end.X && y == end.Y)
+                    {
+                        Console.Write($"END\t");
+                        continue;
+                    }
+                    if (path.Any(point => point.X == x && point.Y == y))
+                    {
+                        Console.Write($"===\t");
+                        continue;
+                    }
+                    Console.Write($"{array[x, y],3}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LeePathSearchAlgorithm
+{
+    //TODO тесты
+    class ExampleProgram
+    {
+        static int[,] testMap = new int[,]{
+            {0,0,0,0,0,0,0},
+            {0,0,1,1,0,0,1},
+            {1,1,1,1,1,0,1},
+            {0,0,1,0,0,0,0},
+            {0,0,1,0,1,0,0},
+            {0,0,0,0,1,0,0},
+        };
+
+        static void Main(string[] args)
+        {
+            //для иллюстрации того, как повёрнуты координаты
+            DebugHelpers.PrintToConsole2DCoordinates(testMap);
+
+            var searcher = new PathSearchLee(PathSearchLee.SearchMethod.Path4);
+            var start = new PathSearchLee.Point(5, 0);
+            var end = new PathSearchLee.Point(0, 6);
+            var path = searcher.Search(testMap, start, end);
+
+            if (path.Length == 0)
+                Console.WriteLine("Нет пути");
+            else
+            {
+                DebugHelpers.PrintPath(testMap, path, start, end);
+                Console.WriteLine("\nПуть по шагам:");
+
+                foreach (var step in path)
+                    Console.WriteLine($"X:{step.X} Y:{step.Y}");
+            }
+        }
+    }
+}
